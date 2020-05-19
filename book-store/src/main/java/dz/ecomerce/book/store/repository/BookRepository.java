d4b8cf4d0e45911a2d5c98663da99beb76121447
salim <salim_iip@outlook.fr>
2020-05-19 20:20:32 +0200

    create angular projet rest client for book

--- book-store/src/main/java/dz/ecomerce/book/store/repository/BookRepository.java
+++ book-store/src/main/java/dz/ecomerce/book/store/repository/BookRepository.java
@@ -1,11 +1,12 @@
 package dz.ecomerce.book.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import dz.ecomerce.book.store.entity.Book;
 
 
-
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
